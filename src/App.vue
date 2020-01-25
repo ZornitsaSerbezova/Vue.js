fa742b11eb8a2829dc519a67d537e4038194f3ed
zornitsaserbezova <zornitsa.serbezova@balkangraph.com>
2020-01-25 14:09:34 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <OrgChart />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import OrgChart from './components/OrgChart.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    OrgChart
   }
 }
 </script>
---
+++ src/components/OrgChart.vue
@@ -0,0 +1,39 @@
+<template>
+    <h1>OrgChart</h1>
+</template>
+
+<script>
+import OrgChart from '@balkangraph/orgchart.js/orgchart'
+
+export default {
+    data: function() {
+ 
+        return {
+            nodes: [
+                { id: 1, name: "Denny Curtis", title: "CEO", img: "https://cdn.balkan.app/shared/2.jpg" },
+                { id: 2, pid: 1, name: "Ashley Barnett", title: "Sales Manager", img: "https://cdn.balkan.app/shared/3.jpg" },
+                { id: 3, pid: 1, name: "Caden Ellison", title: "Dev Manager", img: "https://cdn.balkan.app/shared/4.jpg" },
+                { id: 4, pid: 2, name: "Elliot Patel", title: "Sales", img: "https://cdn.balkan.app/shared/5.jpg" },
+                { id: 5, pid: 2, name: "Lynn Hussain", title: "Sales", img: "https://cdn.balkan.app/shared/6.jpg" },
+                { id: 6, pid: 3, name: "Tanner May", title: "Developer", img: "https://cdn.balkan.app/shared/7.jpg" },
+                { id: 7, pid: 3, name: "Fran Parsons", title: "Developer", img: "https://cdn.balkan.app/shared/8.jpg" }
+            ]
+        } 
+    },
+
+
+  methods: {
+    orgchart: function() {
+        var chart = new OrgChart(document.getElementById("tree"), {
+          nodeBinding: {
+            field_0: "name",
+            field_1: "title"          
+          },
+        });
+        chart.load(this.nodes);
+        return this.chart;
+    }
+
+  }
+}
+</script>
